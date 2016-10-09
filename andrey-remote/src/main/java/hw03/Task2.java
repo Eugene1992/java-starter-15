113004bb5f1d0736be62a86a7f2c6420713b2438
Yeremenkoay <yeremenko.andrey@bk.ru>
2016-10-09 23:45:39 +0300

    first commit

---
+++ andrey-remote/src/main/java/hw03/Task2.java
@@ -0,0 +1,18 @@
+package hw03;
+
+import java.util.Scanner;
+
+/**
+ * Создать программу, выводящую на экран ближайшее к 10 из двух чисел, введенных с консоли.
+ * Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число : ");
+        int number1 = sc.nextInt();
+        System.out.println("Введите второе число : ");
+        int number2 = sc.nextInt();
+
+    }
+}
