9ded272f04b0f380c9507ae0aeb5dc2a88a80e2a
Yeremenkoay <yeremenko.andrey@bk.ru>
2016-10-10 20:05:48 +0300

    Add hw03.

---
+++ andrey-remote/src/main/java/hw03/CW01.java
@@ -0,0 +1,13 @@
+package hw03;
+
+/**
+ * Created by Андрей on 10.10.2016.
+ */
+public class CW01 {
+    public static void main(String[] args) {
+        for(int i = 100; i <=1000; i+=2 ){
+            System.out.println(i);
+        }
+    }
+    }
+
