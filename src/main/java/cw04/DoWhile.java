0a4589b5ea3af9b301d3b4c13f7991d9d9f98a41
mankunianeth <deyneko55@gmail.com>
2016-10-10 21:10:58 +0300

    Add hw04.

---
+++ src/main/java/cw04/DoWhile.java
@@ -0,0 +1,18 @@
+package cw04;
+
+/**
+ * Created by evgeniy on 10/10/16.
+ */
+public class DoWhile {
+    public static void main(String[] args) {
+        int counter = 10;
+
+        while (counter > 10) {
+            System.out.println("Hello");
+        }
+
+        do {
+            System.out.println("Hello");
+        } while (counter > 10);
+    }
+}
---
+++ src/main/java/cw04/Test.java
@@ -0,0 +1,12 @@
+package cw04;
+
+/**
+ * Created by evgeniy on 10/10/16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int k = 10;
+        System.out.println(k--);
+        System.out.println(--k);
+    }
+}
