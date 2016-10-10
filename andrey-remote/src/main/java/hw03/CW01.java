55993d2bce0bd5852a1306936f17e7e6af9122be
Yeremenkoay <yeremenko.andrey@bk.ru>
2016-10-10 20:26:49 +0300

    Add hw03.

--- andrey-remote/src/main/java/hw03/CW01.java
+++ andrey-remote/src/main/java/hw03/CW01.java
@@ -5,9 +5,11 @@ package hw03;
  */
 public class CW01 {
     public static void main(String[] args) {
-        for(int i = 100; i <=1000; i+=2 ){
-            System.out.println(i);
+        int sum = 0;
+        for (int i = 1; i <= 50; i++) {
+            sum += i;
         }
+        System.out.println(sum);
     }
-    }
+}
 
