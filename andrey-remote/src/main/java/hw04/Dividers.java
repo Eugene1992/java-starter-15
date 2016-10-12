6d31800b143884a13c7e3aa2b03f25780c30b3ab
Yeremenkoay <yeremenko.andrey@bk.ru>
2016-10-12 09:14:11 +0300

    hw04

--- andrey-remote/src/main/java/hw04/Dividers.java
+++ andrey-remote/src/main/java/hw04/Dividers.java
@@ -9,13 +9,11 @@ public class Dividers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число: ");
-        String res = "";
         int number = scanner.nextInt();
         for (int i = number; i > 0; i--) {
             if (number % i == 0) {
-                res += i + " ";
+                System.out.print(i + " ");
             }
         }
-        System.out.println(res);
     }
 }
