e92f9b34425cf55e23939f79e9f0d738f0961824
Yeremenkoay <yeremenko.andrey@bk.ru>
2016-10-14 00:52:32 +0300

    hw04

--- andrey-remote/src/main/java/hw05/UnhappyNumbers.java
+++ andrey-remote/src/main/java/hw05/UnhappyNumbers.java
@@ -8,10 +8,10 @@ public class UnhappyNumbers {
         int result = 0;
         for (int i = 1; i < 999999; i++) {
             int number1 = i / 100000;
-            int number2 = i / 10000 % 10;
-            int number3 = i / 1000 % 10;
-            int number4 = i / 100 % 10;
-            int number5 = i / 10 % 10;
+            int number2 = (i / 10000) % 10;
+            int number3 = (i / 1000) % 10;
+            int number4 = (i / 100) % 10;
+            int number5 = (i / 10) % 10;
             int number6 = i % 10;
             if (number1 == 1 & number1 + number2 == 4) {
                 result++;
